ffacfe7dcaab746609e6345d244529af05fa5ba8
Aleaddine El Aji <87655501+AleaddineElAji@users.noreply.github.com>
2021-11-15 09:28:33 +0100

    BoxUserMail

--- view/userPageTemplate.php
+++ view/userPageTemplate.php
@@ -5,27 +5,27 @@
     <span id="titlePageUser">Bienvenu sur la page utilisateur de <?= $userName ?></span>
     <div id="boxMailUser">
         <form action="index.php" method="GET">
+            <div id="boxContact">
+                <div class="form-group">
+                    <label class="formUser" for="name">Entrez votre nom
+                    <input type="text" name="mail" class="form-control" required>
+                    </label>
+                </div>
+                <div class="form-group">
+                    <label class="formUser" for="name">Entrez votre mail
+                    <input type="text" name="sujet" class="form-control" required>
+                    </label>
+                </div>
+                <div class="form-group">
+                    <label class="formUser" for="name">Entrez votre message
+                        <textarea  id="textMail" name="message" class="form-control" required></textarea>
+                    </label>
+                </div>
 
-
-            <div class="form-group">
-                <label class="formUser" for="name">Entrez votre nom
-                  <input type="text" name="mail" class="form-control" required>
-                </label>
+                <input type="submit" name="envoyer" value="Envoyer" id="btSubmitUser"  class="btn btn-primary d-flex"/>
+                <input type="hidden" name="action" value="sendMessage" />
+                <input type="hidden" name="email" value=<?= $email ?> />
             </div>
-            <div class="form-group">
-                <label class="formUser" for="name">Entrez votre mail
-                  <input type="text" name="sujet" class="form-control" required>
-                </label>
-            </div>
-            <div class="form-group">
-                <label class="formUser" for="name">Entrez votre message
-                    <textarea  id="textNul" name="message" class="form-control" required></textarea>
-                </label>
-            </div>
-
-            <input type="submit" name="envoyer" value="Envoyer" id="btSubmitUser"  class="btn btn-primary d-flex"/>
-            <input type="hidden" name="action" value="sendMessage" />
-            <input type="hidden" name="email" value=<?= $email ?> />
         </form>
     </div>    
 </div>
